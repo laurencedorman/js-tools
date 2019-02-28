ee8d5e172a979a4f6414800c703e14c2949033b4
nacho.lopez <nacho.lopez@manomano.com>
2019-02-28 10:12:34 +0000

    Don't donwload translations before start dev envrionment.

--- packages/webpack/scripts/start.js
+++ packages/webpack/scripts/start.js
@@ -5,12 +5,8 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-const fs = require('fs');
-const path = require('path');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const { phraseApp, paths } = require('@manomano/project-settings');
-const { downloadTranslations } = require('@manomano/utils');
 
 const settings = require('../settings');
 const config = require('../config.js');
@@ -21,33 +17,19 @@ const argv = process.argv.slice(2);
 const parsedArgs = require('minimist')(argv);
 const { lang } = parsedArgs;
 
-downloadTranslations(phraseApp).then(translations => {
-  if (!fs.existsSync(paths.appTranslations)) {
-    fs.mkdirSync(paths.appTranslations);
+const compiler = webpack(config(lang));
+const devServer = new WebpackDevServer(compiler, devServerConfig);
+
+devServer.listen(settings.devServer.port, settings.devServer.host, err => {
+  if (err) {
+    return console.log(err);
   }
+  console.log('Starting the development server...\n');
+});
 
-  translations.forEach(({ language, translations }) => {
-    fs.writeFileSync(
-      path.join(paths.appTranslations, `${language}.json`),
-      JSON.stringify(translations, null, 2),
-      'utf8'
-    );
-  });
-
-  const compiler = webpack(config(lang));
-  const devServer = new WebpackDevServer(compiler, devServerConfig);
-
-  devServer.listen(settings.devServer.port, settings.devServer.host, err => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Starting the development server...\n');
-  });
-
-  ['SIGINT', 'SIGTERM'].forEach(function(sig) {
-    process.on(sig, function() {
-      devServer.close();
-      process.exit();
-    });
+['SIGINT', 'SIGTERM'].forEach(function(sig) {
+  process.on(sig, function() {
+    devServer.close();
+    process.exit();
   });
 });
