4f0295592448107b556aced8803d7502449cb877
nacho.lopez <nacho.lopez@manomano.com>
2019-03-12 15:06:18 +0000

    Add babel cache for the webpack build

--- packages/babel/webpack-loader.js
+++ packages/babel/webpack-loader.js
@@ -2,7 +2,7 @@ module.exports = {
   loader: require.resolve('babel-loader'),
   options: {
     presets: [require.resolve('./preset')],
-    cacheDirectory: false,
+    cacheDirectory: true,
     cacheCompression: false,
     compact: false,
     babelrc: false,
