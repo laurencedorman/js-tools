2b7b05f20fd8e7cb35723c7a8aa96418a138309e
Pierre TROLLE <pierre.trolle@manomano.com>
2019-04-12 16:42:58 +0200

    without global

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,29 +2,10 @@
 
 #!/bin/sh
 
-# Package (aka module) release script.
-# Refer to ../releasing.adoc for details about how this script works.
-
-#if [ -z $RELEASE_BRANCH ]; then RELEASE_BRANCH=master; fi
-
-# don't run if this branch is behind the branch from which we're releasing
-#if [ "$(git merge-base --fork-point $RELEASE_BRANCH $CI_COMMIT_SHA)" != "$(git rev-parse $RELEASE_BRANCH)" ]; then
-#  echo $CI_COMMIT_REF_NAME is behind $RELEASE_BRANCH. This could indicate this release was already published. Aborting.
-#  exit 0
-#fi
-
-# set up SSH auth using ssh-agent
-mkdir -p -m 700 $HOME/.ssh
-ssh-keygen -F gitlab.com >/dev/null 2>&1 || ssh-keyscan -H -t rsa gitlab.com >> $HOME/.ssh/known_hosts 2>/dev/null
-eval $(ssh-agent -s) >/dev/null
-echo -n "$RELEASE_SSH_PRIV_KEY" | ssh-add -
-
-# clone the branch from which we're releasing and switch to it
-#git branch -f $RELEASE_BRANCH origin/$RELEASE_BRANCH
-#git clone -b $RELEASE_BRANCH --no-local . build/$CI_PROJECT_NAME
-#cd build/$CI_PROJECT_NAME
-
 # configure git to push changes
+git remote set-url origin https://${GITLAB_CI_USER}:${GITLAB_CI_TOKEN}@git.manomano.tech/core-utils/js-tools.git
+git config user.email "$GIT_AUTHOR_EMAIL"
+git config user.name "$GIT_AUTHOR_NAME"
 git remote set-url origin "git@git.manomano.tech:core-utils/js-tools.git"
 git config user.email "$RELEASE_GIT_EMAIL"
 git config user.name "$RELEASE_GIT_NAME"
