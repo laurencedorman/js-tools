34962b47d634c58223ac92db099225fd8aa8aa9a
matthieu <matthieu@manomano.com>
2019-06-06 14:47:53 +0200

    fix(webpack,project-settings): Changed project alias to relative paths to prevent babel plugin module resolver to insert absolute paths

--- packages/project-settings/alias.js
+++ packages/project-settings/alias.js
@@ -1,22 +1,19 @@
-const path = require('path');
-const { manoConfig, composeObjectValues } = require('@manomano/utils');
+const { manoConfig } = require('@manomano/utils');
 
 const defaultAlias = {
-  components: 'src/components',
-  contexts: 'src/contexts',
-  modules: 'src/modules',
-  pages: 'src/pages',
-  shapes: 'src/shapes',
-  services: 'src/services',
-  store: 'src/store',
-  utils: 'src/utils',
+  components: './src/components',
+  contexts: './src/contexts',
+  modules: './src/modules',
+  pages: './src/pages',
+  shapes: './src/shapes',
+  services: './src/services',
+  store: './src/store',
+  utils: './src/utils',
 };
 
 const projectAlias = manoConfig.alias || {};
 
-const updater = entry => path.resolve(process.cwd(), entry);
-
 module.exports = {
-  ...composeObjectValues(defaultAlias, updater),
-  ...composeObjectValues(projectAlias, updater),
+  ...defaultAlias,
+  ...projectAlias,
 };
